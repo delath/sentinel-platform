0f81cf5232ced071cf63a160835cb6ed0a31f730
Delath <delath.707@gmail.com>
2025-11-29 18:38:52 +0100

    Apply spotless rules

--- sentinel-generator/src/main/java/dev/delath/sentinel/events/TransactionEvent.java
+++ sentinel-generator/src/main/java/dev/delath/sentinel/events/TransactionEvent.java
@@ -1,6 +1,17 @@
 package dev.delath.sentinel.events;
 
-public record TransactionEvent(String transactionId, String userId, String cardToken, String cardBin, long amount, String currency, MerchantDetails merchantDetails, Location location, String ipAddress, String timestamp) {
-    public record MerchantDetails(String merchantId, String mcc, String name) {}
-    public record Location(Double lat, Double lon) {}
+public record TransactionEvent(
+    String transactionId,
+    String userId,
+    String cardToken,
+    String cardBin,
+    long amount,
+    String currency,
+    MerchantDetails merchantDetails,
+    Location location,
+    String ipAddress,
+    String timestamp) {
+  public record MerchantDetails(String merchantId, String mcc, String name) {}
+
+  public record Location(Double lat, Double lon) {}
 }
