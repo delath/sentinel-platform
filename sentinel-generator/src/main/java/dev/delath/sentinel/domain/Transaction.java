252bae5d3bbe93d86a9e77b36b9a790150250127
Delath <delath.707@gmail.com>
2025-11-30 19:23:22 +0100

    Change amount type to long

--- sentinel-generator/src/main/java/dev/delath/sentinel/domain/Transaction.java
+++ sentinel-generator/src/main/java/dev/delath/sentinel/domain/Transaction.java
@@ -1,14 +1,13 @@
 package dev.delath.sentinel.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.math.BigDecimal;
 
 public record Transaction(
         @JsonProperty("transaction_id") String transactionId,
         @JsonProperty("user_id") String userId,
         @JsonProperty("card_token") String cardToken,
         @JsonProperty("card_bin") String cardBin,
-        BigDecimal amount,
+        long amount,
         String currency,
         @JsonProperty("merchant_details") MerchantDetails merchantDetails,
         Location location,
